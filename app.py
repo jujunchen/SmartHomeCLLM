7e24195355c5de5bca693f286faeae1d3763eeeb
chenjujun <0431cjj@163.com>
2024-03-12 13:17:05 +0800

    test

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-import gradio as gr  
-  
-def echo(text):  
-    return text  
-  
-iface = gr.Interface(fn=echo, inputs="text", outputs="text")  
-iface.launch()
+import gradio as gr
+
+# 输入name字符串，输出Hello {name}!字符串
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=gr.Textbox(lines=2, placeholder="Name Here..."),
+    outputs="text",
+    allow_flagging="never",
+)
+if __name__ == "__main__":
+    demo.launch()
