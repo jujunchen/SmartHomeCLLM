6aed0d76869d66bc888473c1778c638686d20be7
chenjujun <0431cjj@163.com>
2024-03-15 14:34:23 +0800

    修改

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import gradio as gr  
 import requests  
   
-def process_input(user_input):  
+def process_input(user_input, user_input2):  
     try: 
         # 构造API请求  
-        url = "https://smarthome-dev.gtdreamlife.com/smartlife/chat"  
+        url = "http://183.129.211.90:22323/smartlife/chat"  
         headers = {"Content-Type": "application/json"}  # 根据你的API需求调整  
         data = {"messages": user_input}  # 构造请求体，这里假设API需要一个名为"param"的参数  
         
@@ -22,10 +22,10 @@ def process_input(user_input):
 # 创建Gradio界面  
 iface = gr.Interface(  
     fn=process_input,  
-    inputs=gr.Textbox(label="输入指令"),  
+    inputs=gr.Dropdown(["打开主卧灯", "关闭主卧灯"], label="选择指令"),
     outputs=gr.JSON(), 
+    description= "做为演示，仅支持两个指令，后续我们将支持更多设备指令",
     title="绿城智能家居指令大模型Demo"  
 )
 
-# 启动界面  
-iface.launch()
+iface.launch()  # 启动界面
